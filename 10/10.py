5395a30ff6c08a95e52a41ffeaa4d8628ee53895
geceavsar <geceavsar@gmail.com>
2023-07-19 22:10:21 +0300

    misunderstood q10, added the correct one

--- 10/10.py
+++ 10/10.py
@@ -1,5 +1,40 @@
 '''
-a = a = [1, 11, 21, 1211, 111221, 
+a = [1, 11, 21, 1211, 111221, 
+
+a[0] = 1
+a[1] = 1 "1" (read how many times a char is present in the previous element) -> 11
+a[2] = 2 "1"s -> 21
+a[3] = 1 "2" and 1 "1" -> 1211
+...
+
+'''
+
+n = 30
+a = ["1"]
+for i in range(1, n+1):
+    char_table = []
+    prev = a[i-1]
+    result = ""
+    for char in prev:
+        if not char_table or char_table[-1][0] != char:
+            char_table.append(list([char, 1]))
+        else:
+            char_table[-1][1] += 1
+
+    for j in char_table:
+        result += str(j[1]) + j[0]
+    a.append(result)
+
+print(len(a[30]))
+
+# next -> http://www.pythonchallenge.com/pc/return/5808.html
+
+
+# I've totally misunderstood the pattern first and came up with
+# another pattern. Yet it applies too. Really.
+
+'''
+a = [1, 11, 21, 1211, 111221, 
 
 0 -> 1
 1 -> 1 1
@@ -11,7 +46,7 @@ a = a = [1, 11, 21, 1211, 111221,
 n -> a[0] a[1] ... a[n-3] 2 a[n-2] 
 
 '''
-
+'''
 def nth_element(n):
     if n < 0: return ""
     if n == 0: return "1"
@@ -22,3 +57,4 @@ def nth_element(n):
     return result + "2" + nth_element(n-2)
 
 print("length of 30th element is:", len(nth_element(30)))
+'''
